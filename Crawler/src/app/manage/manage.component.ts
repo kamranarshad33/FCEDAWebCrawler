a41964817926c86f9715b521cba09577ed87990a
ciphered <hcfisher12@gmail.com>
2020-05-03 19:48:01 -0400

    New attempt

--- Crawler/src/app/manage/manage.component.ts
+++ Crawler/src/app/manage/manage.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
+  isShow = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleDisplay() {
+    this.isShow = !this.isShow;
   }
 
 }
